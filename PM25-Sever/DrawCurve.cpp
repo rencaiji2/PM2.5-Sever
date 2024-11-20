0d4bd824db8250874727d32668a1897bb3c961d1
rencaiji2 <2086586732@qq.com>
2024-11-20 10:49:13 +0800

    完成了初步的曲线功能

--- PM25-Sever/DrawCurve.cpp
+++ PM25-Sever/DrawCurve.cpp
@@ -7,33 +7,66 @@ DrawCurve::DrawCurve(QWidget *parent) :
     ui(new Ui::DrawCurve)
 {
     ui->setupUi(this);
+
     m_customPlot = new QCustomPlot(this);
     m_customPlot->setParent(ui->widget_customplot);
+    m_customPlot->resize(900,460);
+    m_customPlot->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom | QCP::iSelectPlottables);//添加鼠标拖放支持、滚轮缩放支持
+    m_customPlot->xAxis->setRange(QDateTime::currentSecsSinceEpoch()-10,QDateTime::currentSecsSinceEpoch()+10);
+    m_customPlot->yAxis->setRange(0,1000);
 
+    m_qCPGraph_1 = m_customPlot->addGraph();
+     m_qCPGraph_1->setPen(QPen(Qt::blue));
+    m_qCPGraph_2 = m_customPlot->addGraph();
+     m_qCPGraph_2->setPen(QPen(Qt::yellow));
+    m_qCPGraph_3 = m_customPlot->addGraph();
+     m_qCPGraph_3->setPen(QPen(Qt::red));
+    m_qCPGraph_4 = m_customPlot->addGraph();
+     m_qCPGraph_4->setPen(QPen(Qt::black));
 
-    QVector<double> time(200), value(200);
-        for(int i = 0; i < time.count(); i++)
-        {
-            time[i] = QDateTime::currentSecsSinceEpoch() + i;//方法1：以此刻的时间做X轴（总秒数），一秒一个值
-            //time[i] = QDateTime::currentMsecsSinceEpoch() / 1000.0 + i;//方法2
-            value[i] = qrand() % 10 - 5;//[-5, 5]随机数做y值
-        }//生成模拟数据
+     m_intToQCPGraph.insert(1,m_qCPGraph_1);
+     m_intToQCPGraph.insert(2,m_qCPGraph_2);
+     m_intToQCPGraph.insert(3,m_qCPGraph_3);
+     m_intToQCPGraph.insert(4,m_qCPGraph_4);
 
-        m_customPlot->setInteractions(QCP::iRangeDrag | QCP::iRangeZoom | QCP::iSelectPlottables);//添加鼠标拖放支持、滚轮缩放支持
-        m_customPlot->addGraph();
-        m_customPlot->graph(0)->setData(time, value);//设置数据源
-        m_customPlot->graph(0)->rescaleAxes();
+     QVector<double> temporarily_doublevector;
+     QList<QVector<double>> l1;
+     l1.append(temporarily_doublevector);
+     l1.append(temporarily_doublevector);
+     m_drawData.insert(1,l1);
+     m_drawData.insert(2,l1);
+     m_drawData.insert(3,l1);
+     m_drawData.insert(4,l1);
 
-        QSharedPointer<QCPAxisTickerDateTime> dateTicker(new QCPAxisTickerDateTime);//日期做X轴
-        dateTicker->setDateTimeFormat("yy-MM-dd hh:mm:ss");//日期格式(可参考QDateTime::fromString()函数)
-        m_customPlot->xAxis->setTicker(dateTicker);//设置X轴为时间轴
-     m_customPlot->resize(900,460);
+    QSharedPointer<QCPAxisTickerDateTime> dateTicker(new QCPAxisTickerDateTime);//日期做X轴
+    dateTicker->setDateTimeFormat("yy-MM-dd hh:mm:ss");//日期格式(可参考QDateTime::fromString()函数)
+    m_customPlot->xAxis->setTicker(dateTicker);//设置X轴为时间轴
 
-     // 重绘图表
-     m_customPlot->replot();
+    connect(TcpServer::getInstance(),&TcpServer::sendClientData,this,&DrawCurve::clientDataRcevice);
+    connect(&m_timer, &QTimer::timeout,this,&DrawCurve::on_timer_slot);
+    m_timer.start(10000);
 }
 
 DrawCurve::~DrawCurve()
 {
     delete ui;
+}
+
+void DrawCurve::clientDataRcevice(ClientData data)
+{
+    static QVector<double> time, value;
+    time.push_back(QDateTime::currentSecsSinceEpoch());
+    value.push_back(data.result);
+    if(m_intToQCPGraph.contains(data.deviceNum))
+    {
+        m_drawData[data.deviceNum][0].push_back(QDateTime::currentSecsSinceEpoch());
+        m_drawData[data.deviceNum][1].push_back(data.result);
+        m_intToQCPGraph.value(data.deviceNum)->setData(m_drawData[data.deviceNum][0],m_drawData[data.deviceNum][1]);
+        m_customPlot->xAxis->setRangeUpper(QDateTime::currentSecsSinceEpoch()+10);
+    }
+}
+
+void DrawCurve::on_timer_slot()
+{
+    m_customPlot->replot();
 }
--- PM25-Sever/DrawCurve.h
+++ PM25-Sever/DrawCurve.h
@@ -3,6 +3,7 @@
 
 #include <QWidget>
 #include "qcustomplot.h"
+#include "PublicStruct.h"
 
 namespace Ui {
 class DrawCurve;
@@ -16,9 +17,20 @@ public:
     explicit DrawCurve(QWidget *parent = nullptr);
     ~DrawCurve();
 
+private slots:
+    void clientDataRcevice(ClientData data);
+    void on_timer_slot();
+
 private:
     Ui::DrawCurve *ui;
     QCustomPlot *m_customPlot;
+    QCPGraph *m_qCPGraph_1;
+    QCPGraph *m_qCPGraph_2;
+    QCPGraph *m_qCPGraph_3;
+    QCPGraph *m_qCPGraph_4;
+    QHash<int,QCPGraph *> m_intToQCPGraph;
+    QHash<int,QList<QVector<double>>> m_drawData;
+    QTimer m_timer;
 };
 
 #endif // DRAWCURVE_H
